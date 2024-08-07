76cda9752b412daaddff48d93a0b1f30e16dc709
GuilhermeHLSouza <Guilherme.souza726@etec.sp.gov.br>
2024-08-07 10:41:27 -0300

    Chão funcionando

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ const canvas = document.getElementById('canvas').getContext('2d')
 
 let bg = new Bg(0,0,500,900,'img/ceu1.jpeg') //atribuindo valores aos parametros feitos no construtor: altura, largura, posições e imagem
 let bg2 = new Bg(500,0,500,900,'img/ceu1.jpeg')
-let ground = new Bg(0,0,500,200,'img/gameover.jpeg')
-let ground2 = new Bg(500,700,500,200,'img/gameover.jpeg')
+let ground = new Bg(0,700,500,200,'img/gameover.jpeg')
+let ground2 = new Bg(0,700,500,200,'img/gameover.jpeg')
 
 function draw(){
 bg.draw()
@@ -17,8 +17,8 @@ ground2.draw()
 function update(){
     bg.move(10,-500,0)
     bg2.move(10,0,500)
-    ground.move(7,0,-500)
-    ground2.move(7,0,500)
+    ground.move(10,-500,0)
+    ground2.move(10,0,485)
 }
 
 function main(){ //função tipo o do jogo da abelha para reiniciar a pagina, deve ser praquela movimentação do cenário
