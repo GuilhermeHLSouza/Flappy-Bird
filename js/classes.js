58368ddb7958766f2da9b9888ae606c64c7a38a6
GuilhermeHLSouza <Guilherme.souza726@etec.sp.gov.br>
2024-08-07 10:39:29 -0300

    Código para inserir as imagens e fazer um efeito de movimento

--- js/classes.js
+++ js/classes.js
@@ -6,7 +6,19 @@ class obj{
         this.altura = altura
         this.imagem = imagem
     }
+
+    //daqui pra baixo é codigo novo, gileme
     draw(){
-        let img = new Image(this.imagem)
+        let img = new Image()
+        img.src = this.imagem //variavel que está puxando a imagem lá de cima (a do construtor)
+        canvas.drawImage(img,this.posx,this.posy,this.largura,this.altura) //puxando os parametro do construtor
+    }
+}
+
+class Bg extends obj{ //fazer o efeito paralax
+    move(velocidade,limite,posicao){
+        this.posx -= velocidade
+        if(this.posx < limite)
+        this.posx = posicao
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
 const canvas = document.getElementById('canvas').getContext('2d') 
+//daqui pra baixo tem os códigos novos
 
 
+let bg = new Bg(0,0,500,900,'img/ceu1.jpeg') //atribuindo valores aos parametros feitos no construtor: altura, largura, posições e imagem
+let bg2 = new Bg(500,0,500,900,'img/ceu1.jpeg')
+let ground = new Bg(0,0,500,200,'img/gameover.jpeg')
+let ground2 = new Bg(500,700,500,200,'img/gameover.jpeg')
+
+function draw(){
+bg.draw()
+bg2.draw()
+ground.draw()
+ground2.draw()
+}
+
+function update(){
+    bg.move(10,-500,0)
+    bg2.move(10,0,500)
+    ground.move(7,0,-500)
+    ground2.move(7,0,500)
+}
+
+function main(){ //função tipo o do jogo da abelha para reiniciar a pagina, deve ser praquela movimentação do cenário
+    canvas.clearRect(0,0,500,900)
+    update()
+    draw()    
+}
+
+setInterval(main,100)
