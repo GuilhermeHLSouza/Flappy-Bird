be15cabdc7552a6675776ebc43b6fb5828e50423
GuilhermeHLSouza <Guilherme.souza726@etec.sp.gov.br>
2024-08-14 10:43:49 -0300

    Códigos feitos na terceira aula para a criação da animação do flip bird

--- js/classes.js
+++ js/classes.js
@@ -1,4 +1,6 @@
 class obj{
+    quadro = 0
+    tempo = 0
     constructor(posx,posy,largura,altura,imagem){
         this.posx = posx
         this.posy = posy
@@ -7,18 +9,42 @@ class obj{
         this.imagem = imagem
     }
 
-    //daqui pra baixo é codigo novo, gileme
-    draw(){
+
+      draw(){
         let img = new Image()
         img.src = this.imagem //variavel que está puxando a imagem lá de cima (a do construtor)
         canvas.drawImage(img,this.posx,this.posy,this.largura,this.altura) //puxando os parametro do construtor
-    }
+     }
+
+
+      animacao(vel,limite,nomeImg){ //função para a animação do passaro bird eba 
+        this.tempo +=1;
+        if(this.tempo >= vel){
+            this.tempo = 0
+            this.quadro += 1
+        }
+        if(this.quadro >= limite){
+            this.quadro = 0
+        }
+        this.imagem = 'img/'+nomeImg+this.quadro+'.jpeg'
+     }
 }
 
+
 class Bg extends obj{ //fazer o efeito paralax
-    move(velocidade,limite,posicao){
+     move(velocidade,limite,posicao){
         this.posx -= velocidade
         if(this.posx < limite)
         this.posx = posicao
-    }
+     } 
+   
 }
+
+
+class Ground extends Bg{
+
+}
+class Bird extends obj{
+
+}
+
--- js/script.js
+++ js/script.js
@@ -4,27 +4,31 @@ const canvas = document.getElementById('canvas').getContext('2d')
 
 let bg = new Bg(0,0,500,900,'img/ceu1.jpeg') //atribuindo valores aos parametros feitos no construtor: altura, largura, posições e imagem
 let bg2 = new Bg(500,0,500,900,'img/ceu1.jpeg')
-let ground = new Bg(0,700,500,200,'img/gameover.jpeg')
-let ground2 = new Bg(0,700,500,200,'img/gameover.jpeg')
+let ground = new Ground(0,700,500,200,'img/chao.jpeg')
+let ground2 = new Ground(500,700,500,200,'img/chao.jpeg')
+let bird = new Bird(50,400,63,51, 'img/flip1.jpeg') //inserindo a imagem do bird pra depois fazemo a animação
+
 
 function draw(){
 bg.draw()
 bg2.draw()
 ground.draw()
 ground2.draw()
+bird.draw()
 }
 
 function update(){
-    bg.move(10,-500,0)
-    bg2.move(10,0,500)
-    ground.move(10,-500,0)
-    ground2.move(10,0,485)
+    bg.move(5,-500,0)
+    bg2.move(5,0,500)
+    ground.move(2,-500,0)
+    ground2.move(2,0,500)
+    bird.animacao(7,4,'flip')
 }
 
 function main(){ //função tipo o do jogo da abelha para reiniciar a pagina, deve ser praquela movimentação do cenário
     canvas.clearRect(0,0,500,900)
     update()
-    draw()    
+    draw()
+    requestAnimationFrame(main)
 }
-
-setInterval(main,100)
+main()
