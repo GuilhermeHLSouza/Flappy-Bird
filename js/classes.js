7b9779348eda91a1057ecbc3c43d6b8e8a95af6e
GuilhermeHLSouza <Guilherme.souza726@etec.sp.gov.br>
2024-09-04 10:40:06 -0300

    Criação dos canos, gravidade e pulo do personagem, além dos limites, claro.

--- js/classes.js
+++ js/classes.js
@@ -1,7 +1,7 @@
-class obj{
+class obj {
     quadro = 0
     tempo = 0
-    constructor(posx,posy,largura,altura,imagem){
+    constructor(posx, posy, largura, altura, imagem) {
         this.posx = posx
         this.posy = posy
         this.largura = largura
@@ -10,41 +10,61 @@ class obj{
     }
 
 
-      draw(){
+    draw() {
         let img = new Image()
         img.src = this.imagem //variavel que está puxando a imagem lá de cima (a do construtor)
-        canvas.drawImage(img,this.posx,this.posy,this.largura,this.altura) //puxando os parametro do construtor
-     }
+        canvas.drawImage(img, this.posx, this.posy, this.largura, this.altura) //puxando os parametro do construtor
+    }
 
 
-      animacao(vel,limite,nomeImg){ //função para a animação do passaro bird eba 
-        this.tempo +=1;
-        if(this.tempo >= vel){
+    animacao(vel, limite, nomeImg) { //função para a animação do passaro bird eba 
+        this.tempo += 1;
+        if (this.tempo >= vel) {
             this.tempo = 0
             this.quadro += 1
         }
-        if(this.quadro >= limite){
+        if (this.quadro >= limite) {
             this.quadro = 0
         }
-        this.imagem = 'img/'+nomeImg+this.quadro+'.jpeg'
-     }
+        this.imagem = 'img/' + nomeImg + this.quadro + '.jpeg'
+    }
 }
 
 
-class Bg extends obj{ //fazer o efeito paralax
-     move(velocidade,limite,posicao){
+class Bg extends obj { //fazer o efeito paralax
+    move(velocidade, limite, posicao) {
         this.posx -= velocidade
-        if(this.posx < limite)
-        this.posx = posicao
-     } 
-   
-}
-
-
-class Ground extends Bg{
+        if (this.posx < limite)
+            this.posx = posicao
+    }
 
 }
-class Bird extends obj{
 
+
+class Ground extends Bg {
+
+}
+class Bird extends obj {
+    velocidade = 2
+    gravidade = 1
+
+    //Efeito de gravidade
+    grav() {
+        if (this.velocidade < 10) {
+            this.velocidade += this.gravidade
+        }
+        this.posy += this.velocidade
+    }
+    limite(){
+        //Limite de queda
+        if (this.posy >= 660 ) {
+            this.posy = 661
+        }
+        //Limite da subida
+        if (this.posy <= 0 ) {
+            this.posy = 1
+        }
+    }
+    //this.posy += 1
 }
 
--- js/script.js
+++ js/script.js
@@ -1,32 +1,41 @@
-const canvas = document.getElementById('canvas').getContext('2d') 
+const canvas = document.getElementById('canvas').getContext('2d')
 //daqui pra baixo tem os códigos novos
 
 
-let bg = new Bg(0,0,500,900,'img/ceu1.jpeg') //atribuindo valores aos parametros feitos no construtor: altura, largura, posições e imagem
-let bg2 = new Bg(500,0,500,900,'img/ceu1.jpeg')
-let ground = new Ground(0,700,500,200,'img/chao.jpeg')
-let ground2 = new Ground(500,700,500,200,'img/chao.jpeg')
-let bird = new Bird(50,400,63,51, 'img/flip1.jpeg') //inserindo a imagem do bird pra depois fazemo a animação
+let bg = new Bg(0, 0, 500, 900, 'img/ceu1.jpeg') //atribuindo valores aos parametros feitos no construtor: altura, largura, posições e imagem
+let bg2 = new Bg(500, 0, 500, 900, 'img/ceu1.jpeg')
+let ground = new Ground(0, 700, 500, 200, 'img/chao.jpeg')
+let ground2 = new Ground(500, 700, 500, 200, 'img/chao.jpeg')
+let bird = new Bird(50, 400, 63, 51, 'img/flip1.jpeg') //inserindo a imagem do bird pra depois fazemo a animação
+let cano1 = new obj(300,400,96,300,'img/canobottom.jpeg')
+let cano2 = new obj(300,0,96,300,'img/canoup.jpeg')
 
+document.addEventListener('click', function(e){
+    bird.velocidade -= 20
+})
 
-function draw(){
-bg.draw()
-bg2.draw()
-ground.draw()
-ground2.draw()
-bird.draw()
+function draw() {
+    bg.draw()
+    bg2.draw()
+    ground.draw()
+    ground2.draw()
+    bird.draw()
+    cano1.draw()
+    cano2.draw()
 }
 
-function update(){
-    bg.move(5,-500,0)
-    bg2.move(5,0,500)
-    ground.move(2,-500,0)
-    ground2.move(2,0,500)
-    bird.animacao(7,4,'flip')
+function update() {
+    bg.move(5, -500, 0)
+    bg2.move(5, 0, 500)
+    ground.move(2, -500, 0)
+    ground2.move(2, 0, 500)
+    bird.animacao(7, 4, 'flip')
+    bird.grav()
+    bird.limite()
 }
 
-function main(){ //função tipo o do jogo da abelha para reiniciar a pagina, deve ser praquela movimentação do cenário
-    canvas.clearRect(0,0,500,900)
+function main() { //função tipo o do jogo da abelha para reiniciar a pagina, deve ser praquela movimentação do cenário
+    canvas.clearRect(0, 0, 500, 900)
     update()
     draw()
     requestAnimationFrame(main)
